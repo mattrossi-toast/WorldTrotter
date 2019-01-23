63c67b459339cda6b79c46c30e05c1fc8ec3d126
Rossi <008000100@N358-C2.local>
2019-01-23 18:52:01 -0500

    Initial Commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Rossi, Matthew Th on 1/23/19.
+//  Copyright © 2019 Rossi, Matthew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
